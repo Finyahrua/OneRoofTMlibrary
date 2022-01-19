f9abbe634b1a4f6512716ff27f72307aa6404599
Finyahrua <finaddy@gmail.com>
2022-01-19 19:53:11 +0300

    created the first app screen called Categories.js

---
+++ screens/CategoriesScreen.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import { View, Text } from 'react-native'
+import CategoriesBanner from '../components/categoriesScreenComps/Banner'
+
+const CategoriesScreen = () => {
+    return (
+        <View>
+            <CategoriesBanner/>
+        </View>
+    )
+}
+
+export default CategoriesScreen
