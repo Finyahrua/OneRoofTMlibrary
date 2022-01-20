7823445861b1cf0a8ae9f663e43d7e34bd1942f7
Finyahrua <finaddy@gmail.com>
2022-01-20 19:07:59 +0300

    modified the categories data json

---
+++ assets/data/categoriesData.js
@@ -0,0 +1,63 @@
+const categoriesData = [
+  {
+    id: 1,
+    title: "Halls & Decorations",
+    image: require("../images/hallanddeco.png"),
+  },
+  {
+    id: 2,
+    title: "Video and Photos",
+    image: require("../images/videonphoto.png"),
+  },
+  {
+    id: 3,
+    title: "Catering",
+    image: require("../images/catering.png"),
+  },
+  {
+    id: 4,
+    title: "Makeup Artist",
+    image: require("../images/makeup.png"),
+  },
+  {
+    id: 5,
+    title: "Car Rental",
+    image: require("../images/carrental.png"),
+  },
+  {
+    id: 6,
+    title: "MC's",
+    image: require("../images/mc's.png"),
+  },
+  {
+    id: 7,
+    title: "Cake",
+    image: require("../images/cakes.png"),
+  },
+  {
+    id: 8,
+    title: "Saloon",
+    image: require("../images/saloon.png"),
+  },
+  {
+    id: 9,
+    title: "Barbershop",
+    image: require("../images/barber.png"),
+  },
+  {
+    id: 10,
+    title: "Cards",
+    image: require("../images/cards.png"),
+  },
+  {
+    id: 11,
+    title: "Cleaners",
+    image: require("../images/cleaners.png"),
+  },
+  {
+    id: 12,
+    title: "Music",
+    image: require("../images/music.png"),
+  },
+];
+export default categoriesData;
