9a8fe4248e8b2f5735828d1647dd7d917bdf1d96
Finyahrua <finaddy@gmail.com>
2022-01-20 19:07:03 +0300

    added flex 1 to the parrent component to allow the whole flatlist to be rendered

--- screens/CategoriesScreen.js
+++ screens/CategoriesScreen.js
@@ -1,11 +1,13 @@
 import React from 'react'
 import { View, Text } from 'react-native'
 import CategoriesBanner from '../components/categoriesScreenComps/Banner'
+import Categories from '../components/categoriesScreenComps/Categories'
 
 const CategoriesScreen = () => {
     return (
-        <View>
+        <View style={{flex:1}}>
             <CategoriesBanner/>
+            <Categories/>
         </View>
     )
 }
